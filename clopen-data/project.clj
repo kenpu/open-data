bfd0b2e7ab7f56bc50af281aad22add54445b416
Ken Pu <kenpu@cs.toronto.edu>
2015-05-21 18:59:39 -0400

    download datasets

--- clopen-data/project.clj
+++ clopen-data/project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.aviso/pretty "0.1.17"]
+                 [io.aviso/pretty "0.1.18"]
                  [clj-http "1.1.2"]
-                 [org.clojure/data.json "0.2.6"]]
-  :repl-options {:nrepl-middleware [io.aviso.nrepl/pretty-middleware]})
+                 [org.clojure/data.json "0.2.6"]])
--- clopen-data/src/clopen_data/open_canada/csv.clj
+++ clopen-data/src/clopen_data/open_canada/csv.clj
@@ -1,6 +1,7 @@
 (ns clopen-data.open-canada.csv
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.java.shell :as shell]))
 
 (def url "http://open.canada.ca/data/en/dataset")
 (def action-url "http://open.canada.ca/data/api/action/package_show")
@@ -9,6 +10,9 @@
   [page]
   (client/get url {:query-params {:res_format "CSV" :page page}}))
 
+;
+; (file-csv-index-pages "dl/index-pages/csv-index")
+;
 (defn file-csv-index-pages
   [filename]
   (dorun
@@ -25,7 +29,9 @@
     (do
       (map #(% 1) (re-seq pat content)))))
 
-
+;
+; (file-dataset-ids "dl/csv-ids.json"
+;
 (defn file-dataset-ids [out-filename]
   (let [ids (flatten 
               (for [p (range 1 347)] 
@@ -37,6 +43,11 @@
 (defn exists [filename]
   (.exists (clojure.java.io/as-file filename)))
 
+;
+; (dl-json-descriptors)
+; this causes timeout due to bandwidth limiter
+; so will require numerous retries
+;
 (defn dl-json-descriptors
   []
   (let [idx-file "dl/csv-ids.json"
@@ -56,7 +67,48 @@
                   (Thread/sleep 100)
                   (spit filename (res :body)))))))))))
 
+;
+; (build-urls "dl/download.txt")
+;
+(defn build-urls
+  [out-filename]
+  (with-open [out (clojure.java.io/writer out-filename)]
+    (dorun
+      (for [filename (->> "dl/json" (new java.io.File) .list)]
+        (do
+          (println "filename:" filename)
+          (let [data (json/read-str (slurp (str "dl/json/" filename)))]
+            (dorun
+              (for [r ((data "result") "resources") :when (= (r "format") "CSV")]
+                (let [id (r "id")
+                      url (r "url")]
+                  (println id)
+                  (.write out (format "%s\n" url)))))))))))
 
 
-        
+(defn download
+  [prefix url]
+  (println "trying:" url)
+  (if (exists (str prefix "/success"))
+    (println "skip" prefix url)
+    (do
+      (clojure.java.io/make-parents prefix)
+      (let [result (shell/sh "wget" "-P" prefix url)]
+        (if (zero? (result :exit))
+          (do
+            (println "Done" prefix url)
+            (spit (str prefix "/success") "ok"))
+          (throw (Exception. (str prefix " failed " (:err result)))))))))
+
+;
+; (...)
+;
+(defn get-files
+  []
+  (with-open [rdr (clojure.java.io/reader "dl/download.txt")]
+    (dorun
+      (for [[i url] (map-indexed vector (line-seq rdr))]
+        (try 
+          (download (str "dl/files/" i) url)
+          (catch Exception e nil))))))
 
